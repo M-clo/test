2456c60f8dd1503db623b8a91f1be92d8b5ca9f6
Jin Haihui <2415484771@qq.com>
2025-12-04 20:47:39 +0800

    4.2: 第二次修改hellogit.cpp,新增变量num及输出语句

--- hellogit.cpp
+++ hellogit.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int main() {
     cout << "Hello Git!" << endl;
-    cout << "第一次修改a.cpp" << endl;  // 新增输出语句
+    cout << "第一次修改a.cpp" << endl;
+    int num = 10;  // 新增变量定义
+    cout << "第二次修改：新增变量num=" << num << endl;  // 新增输出
     return 0;
 }
